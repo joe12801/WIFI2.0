fdb9656d524dc2e7b27c2105a4a575110b980a9f
joe12801 <70465495+joe12801@users.noreply.github.com>
2023-07-05 18:15:56 +0800

    Add files via upload

---
+++ network/wificonfig.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+PATH_TO_PYTHON_FILE="/home/user/wifi/main.py"
+PORT_TO_CHECK=8080
+
+CHECK_INTERVAL=60  # 延时检查的时间间隔，单位为秒
+DELAY=3  # 命令之间的延时，单位为秒
+
+connected=true
+
+check_network_connection() {
+    local response=$(curl -s -o /dev/null -w "%{http_code}" "http://www.baidu.com")
+    sleep 2
+    if [ "$response" = "200" ]; then
+        connected=true
+        echo "Turning on green internet LED..."
+        echo "default-on" > /sys/class/leds/green:internet/trigger
+    else
+        connected=false
+        echo "Turning off green internet LED..."
+        echo "none" > /sys/class/leds/green:internet/trigger
+    fi
+}
+
+#while true; do
+    check_network_connection
+    
+    if [[ "$connected" == false ]]; then
+        port_in_use=$(lsof -i :$PORT_TO_CHECK)
+        if [[ -n "$port_in_use" ]]; then
+            echo "Port $PORT_TO_CHECK is already in use. Terminating the process..."
+            pid=$(echo "$port_in_use" | awk 'NR==2{print $2}')
+            sleep 5
+            kill -9 $pid
+            echo "Process with PID $pid has been terminated."
+        else
+            echo "Port $PORT_TO_CHECK is available."
+        fi
+        
+        echo "Connecting to Wi-Fi hotspot..."
+        nmcli d wifi hotspot ifname wlan0 ssid 4G-WIFI-blue password 12345678
+        sleep $DELAY
+        python3 "${PATH_TO_PYTHON_FILE}"
+    else
+        echo "No further action needed."
+    fi
+    #sleep $CHECK_INTERVAL  
+#done
